eb7e3fb4033f1d87daa7d62d47e29a1e95a2fdb2
WebcrazyDylan <webcrazy@naver.com>
2021-04-07 16:49:28 -0700

    #8.8 Themes on Styled Components

--- src/App.js
+++ src/App.js
@@ -1,31 +1,18 @@
 import { useReactiveVar } from "@apollo/client";
-import {
-  BrowserRouter as Router,
-  Redirect,
-  Route,
-  Switch
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
 import NotFound from "./screens/NotFound";
 import { darkModeVar, isLoggedInVar } from "./apollo";
 import { ThemeProvider } from "styled-components";
-
-const lightTheme = {
-  fontColor: "#2c2c2c",
-  bgColor: "lightgray"
-};
-
-const darkTheme = {
-  fontColor: "lightgray",
-  bgColor: "#2c2c2c"
-};
+import { darkTheme, GlobalStyles, lightTheme } from "./styles";
 
 function App() {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
   const darkMode = useReactiveVar(darkModeVar);
   return (
     <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <GlobalStyles />
       <Router>
         <Switch>
           <Route path="/" exact>
