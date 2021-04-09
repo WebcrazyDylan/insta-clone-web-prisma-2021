218dd7f0b29074ef15f1b1a949a95d02bb68efe3
WebcrazyDylan <webcrazy@naver.com>
2021-04-09 00:04:45 -0700

    #11.3 Avatar (08:34)

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -5,6 +5,7 @@ import { isLoggedInVar, logUserOut } from "../apollo";
 const ME_QUERY = gql`
   query me {
     me {
+      id
       username
       avatar
     }
@@ -23,6 +24,7 @@ function useUser() {
       logUserOut();
     }
   }, [data]);
-  return;
+
+  return { data };
 }
 export default useUser;
