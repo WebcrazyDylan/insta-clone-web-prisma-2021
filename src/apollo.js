73284eead34205c7fde1081a9b3c17cf4556e677
WebcrazyDylan <webcrazy@naver.com>
2021-04-08 18:57:12 -0700

    #10.11 Login part Two (10:49)

--- src/apollo.js
+++ src/apollo.js
@@ -1,6 +1,19 @@
 import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
 
-export const isLoggedInVar = makeVar(false);
+const TOKEN = "token";
+
+export const isLoggedInVar = makeVar(Boolean(localStorage.getItem(TOKEN)));
+
+export const logUserIn = (token) => {
+  localStorage.setItem(TOKEN, token);
+  isLoggedInVar(true);
+};
+
+export const logUserOut = () => {
+  localStorage.removeItem(TOKEN);
+  isLoggedInVar(false);
+};
+
 export const darkModeVar = makeVar(false);
 
 export const client = new ApolloClient({
