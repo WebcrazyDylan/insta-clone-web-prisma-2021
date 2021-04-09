4a8fb1f20d70f58b4558496dd43afbcd89e603b8
WebcrazyDylan <webcrazy@naver.com>
2021-04-08 23:49:27 -0700

    #11.2 Header part Three (12:16)

---
+++ src/components/Header.js
@@ -0,0 +1,84 @@
+import { useReactiveVar } from "@apollo/client";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faCompass, faUser } from "@fortawesome/free-regular-svg-icons";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { isLoggedInVar } from "../apollo";
+import useUser from "../hooks/useUser";
+import routes from "../routes";
+
+const SHeader = styled.header`
+  width: 100%;
+  border-bottom: 1px solid ${(props) => props.theme.borderColor};
+  background-color: ${(props) => props.theme.bgColor};
+  padding: 18px 0px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  max-width: 930px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Column = styled.div``;
+
+const Icon = styled.span`
+  margin-left: 15px;
+`;
+
+const Btn = styled.span`
+  background-color: ${(props) => props.theme.accent};
+  color: white;
+  border-radius: 4px;
+  padding: 5px 15px;
+  font-weight: 600;
+`;
+
+const Button = styled.span`
+  background-color: ${(props) => props.theme.accent};
+  border-radius: 4px;
+  padding: 4px 15px;
+  color: white;
+  font-weight: 600;
+`;
+
+function Header() {
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
+  const loggedInUser = useUser();
+  return (
+    <SHeader>
+      <Wrapper>
+        <Column>
+          <FontAwesomeIcon icon={faInstagram} size="2x" />
+        </Column>
+        <Column>
+          {isLoggedIn ? (
+            <>
+              <Icon>
+                <FontAwesomeIcon icon={faHome} size="lg" />
+              </Icon>
+              <Icon>
+                <FontAwesomeIcon icon={faCompass} size="lg" />
+              </Icon>
+              <Icon>
+                <FontAwesomeIcon icon={faUser} size="lg" />
+              </Icon>
+            </>
+          ) : (
+            <Link href={routes.home}>
+              <Button>Login</Button>
+            </Link>
+          )}
+        </Column>
+      </Wrapper>
+    </SHeader>
+  );
+}
+export default Header;
---
+++ src/hooks/useUser.js
@@ -0,0 +1,28 @@
+import { gql, useQuery, useReactiveVar } from "@apollo/client";
+import { useEffect } from "react";
+import { isLoggedInVar, logUserOut } from "../apollo";
+
+const ME_QUERY = gql`
+  query me {
+    me {
+      username
+      avatar
+    }
+  }
+`;
+
+function useUser() {
+  const hasToken = useReactiveVar(isLoggedInVar);
+  const { data } = useQuery(ME_QUERY, {
+    skip: !hasToken
+  });
+  console.log(data);
+
+  useEffect(() => {
+    if (data?.me === null) {
+      logUserOut();
+    }
+  }, [data]);
+  return;
+}
+export default useUser;
