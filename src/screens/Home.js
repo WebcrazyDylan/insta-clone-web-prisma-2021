a12a54d8504eae86e5643ee20d407a3c998e8e19
WebcrazyDylan <webcrazy@naver.com>
2021-04-10 00:14:32 -0700

    #11.5 Photo Component part Two (11:59)

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,111 @@
+import { gql, useQuery } from "@apollo/client";
+import {
+  faBookmark,
+  faComment,
+  faHeart,
+  faPaperPlane
+} from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styled from "styled-components";
 import { logUserOut } from "../apollo";
+import Avatar from "../components/Avatar";
+import { FatText } from "../components/shared";
+
+const FEED_QUERY = gql`
+  query seeFeed {
+    seeFeed {
+      id
+      user {
+        username
+        avatar
+      }
+      file
+      caption
+      likes
+      comments
+      createdAt
+      isMine
+    }
+  }
+`;
+
+const PhotoContainer = styled.div`
+  background-color: white;
+  border: 1px solid ${(props) => props.theme.borderColor};
+  margin-bottom: 20px;
+  max-width: 615px;
+`;
+const PhotoHeader = styled.div`
+  padding: 15px;
+  display: flex;
+  align-items: center;
+`;
+
+const Username = styled(FatText)`
+  margin-left: 15px;
+`;
+
+const PhotoFile = styled.img`
+  max-width: 100%;
+`;
+
+const PhotoData = styled.div`
+  padding: 15px;
+`;
+
+const PhotoActions = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  div {
+    display: flex;
+    align-items: center;
+  }
+`;
+
+const PhotoAction = styled.div`
+  margin-right: 10px;
+`;
+
+const Likes = styled(FatText)`
+  margin-top: 15px;
+  display: block;
+`;
 
 function Home() {
+  const { data } = useQuery(FEED_QUERY);
   return (
     <div>
-      <h1>Welcome we did it!</h1>
-      <button onClick={() => logUserOut()}>Log out now!</button>
+      {data?.seeFeed?.map((photo) => (
+        <PhotoContainer key={photo.id}>
+          <PhotoHeader>
+            <Avatar lg url={photo.user.avatar} />
+            <Username>{photo.user.username}</Username>
+          </PhotoHeader>
+          <PhotoFile src={photo.file} />
+          <PhotoData>
+            <PhotoActions>
+              <div>
+                <PhotoAction>
+                  <FontAwesomeIcon size={"2x"} icon={faHeart} />
+                </PhotoAction>
+                <PhotoAction>
+                  <FontAwesomeIcon size={"2x"} icon={faComment} />
+                </PhotoAction>
+                <PhotoAction>
+                  <FontAwesomeIcon size={"2x"} icon={faPaperPlane} />
+                </PhotoAction>
+              </div>
+              <div>
+                <FontAwesomeIcon size={"2x"} icon={faBookmark} />
+              </div>
+            </PhotoActions>
+            <Likes>
+              {photo.likes === 1 ? "1 like" : `${photo.likes} likes`}
+            </Likes>
+          </PhotoData>
+        </PhotoContainer>
+      ))}
     </div>
   );
 }
