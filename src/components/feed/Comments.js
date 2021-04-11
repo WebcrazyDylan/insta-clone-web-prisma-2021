d83aae17106f8557263b71e5f35abdfbb6674df3
WebcrazyDylan <webcrazy@naver.com>
2021-04-10 23:25:42 -0700

    #11.18 Create Comment part Three (07:59)

--- src/components/feed/Comments.js
+++ src/components/feed/Comments.js
@@ -62,11 +62,28 @@ function Comments({ photoId, author, caption, commentNumber, comments }) {
           ...userData.me
         }
       };
+      const newCacheComment = cache.writeFragment({
+        data: newComment,
+        fragment: gql`
+          fragment BSName on Comment {
+            id
+            createdAt
+            isMine
+            payload
+            user {
+              username
+              avatar
+            }
+          }
+        `
+      });
+      //   console.log(newCacheComment);
+
       cache.modify({
         id: `Photo:${photoId}`,
         fields: {
           comments(prev) {
-            return [...prev, newComment];
+            return [...prev, newCacheComment];
           },
           commentNumber(prev) {
             return prev + 1;
